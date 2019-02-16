65e64f3e11e7a0a79f767be7aaaae16645505329
Carlos Denarde <carlosv.denarde@gmail.com>
2019-02-16 18:45:11 -0200

    setup

---
+++ src/main/java/com/groupay/api/GrouPayApp/GrouPayAppApplication.java
@@ -0,0 +1,13 @@
+package com.groupay.api.GrouPayApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouPayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrouPayAppApplication.class, args);
+	}
+
+}
