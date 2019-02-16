61c4e1aeaaffa7689cc1f07b461320c4d7e91a87
Carlos Denarde <carlosv.denarde@gmail.com>
2019-02-16 21:58:44 -0200

    consume zoop

--- src/main/java/com/groupay/api/controller/UserController.java
+++ src/main/java/com/groupay/api/controller/UserController.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,7 +20,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.groupay.api.dto.UserZoopDTO;
 import com.groupay.api.model.User;
 import com.groupay.api.repository.UserRepository;
 
@@ -27,6 +32,12 @@ public class UserController {
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	@Value( "${api.zoop.auth}" )
+	private String zoopAuth;
+	
+	@Value("${api.zoop.createUser.url}")
+	private String zoopCreateUserUri;
 	 
 	@GetMapping("/users")
 	public List<User> getAllUsuarios() {
@@ -36,6 +47,14 @@ public class UserController {
 
 	@PostMapping("/users/create")
 	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", zoopAuth);      
+		headers.set("Content-Type", "application/json"); 
+		HttpEntity<UserZoopDTO> request = new HttpEntity<UserZoopDTO>(new UserZoopDTO(), headers);
+
+		UserZoopDTO response = restTemplate.postForObject(zoopCreateUserUri, request, UserZoopDTO.class);
+		user.setZoopId(response.getId());
 		userRepository.save(user);
 		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
---
+++ src/main/java/com/groupay/api/dto/UserZoopDTO.java
@@ -0,0 +1,17 @@
+package com.groupay.api.dto;
+
+public class UserZoopDTO {
+	
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+
+}
