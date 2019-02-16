3fccf4c23d54f550877c3f89122b7141937db01c
Carlos Denarde <carlosv.denarde@gmail.com>
2019-02-16 18:54:33 -0200

    adjust name package

--- src/main/java/com/groupay/api/GrouPayAppApplication.java
+++ src/main/java/com/groupay/api/GrouPayAppApplication.java
@@ -1,4 +1,4 @@
-package com.groupay.api.GrouPayApp;
+package com.groupay.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
